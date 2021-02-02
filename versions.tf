d3c7819d9bcb1b9c01c05ee57cde032de7ed31b3
Robert de Bock <robert@meinit.nl>
2021-02-02 11:15:21 +0100

    Less comments.

--- versions.tf
+++ versions.tf
@@ -1,11 +1,5 @@
 # Configure the Azure provider
 terraform {
-  # backend "azurerm" {
-  # resource_group_name  = "rg-terraform-cmn-sbx"
-  # storage_account_name = "stterraformcmnsbx"
-  # container_name       = "c-terraform"
-  # key                  = "rg-robertdebock-sbx.tfstate"
-  # }
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
