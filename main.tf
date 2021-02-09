e2cf76ff5d5a1fd711a4e7cb3f04e6721f93325b
Robert de Bock <robert@meinit.nl>
2021-02-09 11:49:18 +0100

    Added a .

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
-# Create a virtual network
+# Create a virtual network.
 resource "azurerm_virtual_network" "vnet" {
     name                = "myTFVnet-robert"
     address_space       = ["10.0.0.0/16"]
