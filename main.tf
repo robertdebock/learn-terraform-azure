02d26d527e98fa1b622265d88b24dd486a19d923
Robert de Bock <robert@meinit.nl>
2021-02-10 08:54:57 +0100

    Split.

--- main.tf
+++ main.tf
@@ -1,19 +1,5 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.26"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-robertdebock-sbx"
+  name     = "rg-robertdeboer-sbx"
   location = "westeurope"
   tags     = {
     costcenter   = "infra"
