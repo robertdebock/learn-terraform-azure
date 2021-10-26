9ab486fdb99ded478196dfbf312de099a45e3eeb
Robert de Bock <robert@meinit.nl>
2021-10-26 15:05:43 +0200

    stuff.

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-robertdebock-sbx"
   location = "westeurope"
   tags     = {
-    costcenter   = "infrastructuur"
-    solution     = "terraform opleiding"
+    costcenter   = "infra"
+    solution     = "education"
     owner        = "Robert de Bock"
     environment  = "sbx"
-    creationdate = "15/06/2021"
+    creationdate = "23/12/2020"
     creator      = "Robert de Bock"
   }
 }
